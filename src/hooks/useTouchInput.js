032ea7feb8165c32ef5ad99be2daf7d42d1a08e2
DEX406 <jakelutz1995@gmail.com>
2026-04-23 21:38:00 +0930

    Update useTouchInput.js

--- src/hooks/useTouchInput.js
+++ src/hooks/useTouchInput.js
@@ -4,7 +4,7 @@ import { RAD_TO_DEG } from '../constants.js';
 import { MIN_ZOOM, MAX_ZOOM } from './useViewport.js';
 
 const TAP_THRESHOLD = 10;
-const LONG_PRESS_MS = 1500;
+const LONG_PRESS_MS = 1000;
 const DOUBLE_TAP_MS = 400;
 
 export function useTouchInput({
